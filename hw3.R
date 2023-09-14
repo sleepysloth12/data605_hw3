037703ddb74cc030e35e21240c6e9c9244c4c212
sleepysloth12 <102617368+sleepysloth12@users.noreply.github.com>
2023-09-13 20:11:50 -0400

    Add files via upload

---
+++ hw3.R
@@ -0,0 +1,26 @@
+# 1
+
+#what is the rank of matrix A?
+
+A=matrix(c(1,2,3,4,-1,0,1,3),nrow=4,ncol=4)
+A
+
+rank_A=rankMatrix(A)
+
+print(rank_A)
+
+#the rank of matrix A is 2 
+
+#2
+
+#{insert text here]}
+
+
+#3
+
+B=matrix(c(1,2,1,3,6,3,2,4,2),ncol=3, nrow=3)
+rank_B=rankMatrix(B)
+rank_B
+
+#The rank of matrix B is 1
+
